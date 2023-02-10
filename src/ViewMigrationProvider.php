51638bf679ee8b62d7bac80d8544525037064410
Mikhail-Mouner <mikhaeelmouner@yahoo.com>
2023-02-10 13:45:45 +0200

    Add Provider File

---
+++ src/ViewMigrationProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MikhailMouner\GenerateViewFile;
+
+use Illuminate\Support\ServiceProvider;
+use MikhailMouner\GenerateViewFile\Console\Commands\MakeViewFile;
+
+class ViewMigrationProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
